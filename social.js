d5fa16e0ae27368079bde334f903bbf9301057ae
PK <pradeepdhital124@gmail.com>
2022-07-18 11:07:00 +1000

    calendar nicely working

--- social.js
+++ social.js
@@ -1,5 +1,39 @@
 const date = new Date();
 
+const events = {
+  "22/6/2022": [
+    {
+      name: "event1",
+      link: "lonk ...",
+    },
+    {
+      name: "event",
+      link: "lonk ...",
+    },
+  ],
+  "26/6/2022": [
+    {
+      name: "dancing programme at macquirie park",
+      link: "link ...",
+    },
+  ],
+  "19/6/2022": [
+    {
+      name: "dancing programme at macquirie park",
+      link: "https://www.instagram.com",
+    },
+  ],
+  "28/6/2022": [
+    {
+      name: "pk",
+      link: "https://www.facebook.com/",
+    },
+  ],
+};
+
+const showEvents = (str) => {
+  console.log(str);
+};
 const renderCalendar = () => {
   date.setDate(1);
 
@@ -10,6 +44,7 @@ const renderCalendar = () => {
     date.getMonth() + 1,
     0
   ).getDate();
+  //working on my part
 
   const prevLastDay = new Date(
     date.getFullYear(),
@@ -18,21 +53,13 @@ const renderCalendar = () => {
   ).getDate();
 
   const firstDayIndex = date.getDay();
+  // console.log(firstDayIndex);
 
   const lastDayIndex = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
     0
   ).getDay();
-
-  //modify
-
-  const events = [
-    {
-      data: "2020-2-20",
-      link: "http:/.....",
-    },
-  ];
 
   const nextDays = 7 - lastDayIndex - 1;
 
@@ -58,31 +85,42 @@ const renderCalendar = () => {
   let days = "";
 
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x + 1}
-    <a class="event_link">pk</a>
-    </div>`;
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
   }
 
   for (let i = 1; i <= lastDay; i++) {
-    if (
-      i === new Date().getDate() &&
-      date.getMonth() === new Date().getMonth()
-    ) {
-      days += `<div class="today">${i}
-      <a class="event_link">pk</a></div>`;
-    } else {
-      days += `<div>${i}<a class="event_link">pk</a></div>`;
+    const fullDate = `${i}/${date.getMonth()}/${date.getFullYear()}`;
+
+    const hasEvent = events[fullDate];
+    const eventLength = hasEvent?.length;
+
+    const today =
+      i === new Date().getDate() && date.getMonth() === new Date().getMonth();
+
+    let msg = "";
+    if (hasEvent) {
+      for (let i = 0; i < hasEvent.length; i++) {
+        msg += `<li>
+        <a href="${hasEvent[i]?.link}" target ="_blank"  >
+        ${hasEvent[i]?.name}</a></li>`;
+      }
     }
+
+    days += `<div class=${today && "today"}>
+    ${i}  <br />
+    <ul>
+    ${msg}
+    <ul>
+    
+    </div>`;
   }
 
   for (let j = 1; j <= nextDays; j++) {
-    days += `<div class="next-date event_link">${j}<a class="event_link">pk</a></div>`;
+    days += `<div class="next-date">${j}</div>`;
   }
   monthDays.innerHTML = days;
-
-  showEvents();
 };
-
+renderCalendar();
 document.querySelector(".prev").addEventListener("click", () => {
   date.setMonth(date.getMonth() - 1);
   renderCalendar();
@@ -92,10 +130,3 @@ document.querySelector(".next").addEventListener("click", () => {
   date.setMonth(date.getMonth() + 1);
   renderCalendar();
 });
-
-const showEvents = () => {
-  const daysButton = document.querySelectorAll(".days > div");
-  console.log(daysButton);
-};
-
-renderCalendar();
